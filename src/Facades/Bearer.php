394c45f22a6546d3a3f817152ed61a97d97f9eff
Ryan Chandler <ryangjchandler@gmail.com>
2021-05-14 15:34:29 +0100

    wip

--- src/Facades/Bearer.php
+++ src/Facades/Bearer.php
@@ -5,6 +5,9 @@ namespace RyanChandler\Bearer\Facades;
 use Illuminate\Support\Facades\Facade;
 use RyanChandler\Bearer\Bearer as BearerManager;
 
+/**
+ * @method static \RyanChandler\Bearer\Models\Token|null find(string $token)
+ */
 class Bearer extends Facade
 {
     protected static function getFacadeAccessor()
