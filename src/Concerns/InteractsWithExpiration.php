7e8c8134c1e8abaac3092b1eba72017babcc283a
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-05-18 08:19:13 -0400

    Remove unnecesary docblocks

--- src/Concerns/InteractsWithExpiration.php
+++ src/Concerns/InteractsWithExpiration.php
@@ -6,9 +6,6 @@ use RyanChandler\Bearer\Models\Token;
 
 trait InteractsWithExpiration
 {
-    /**
-     * Add months to the expiration date.
-     */
     public function addMonths(int $months): Token
     {
         $expires = $this->expires_at ?: now();
@@ -16,9 +13,6 @@ trait InteractsWithExpiration
         return $this->expires($expires->addMonths($months));
     }
 
-    /**
-     * Add weeks to the expiration date.
-     */
     public function addWeeks(int $weeks): Token
     {
         $expires = $this->expires_at ?: now();
@@ -26,9 +20,6 @@ trait InteractsWithExpiration
         return $this->expires($expires->addWeeks($weeks));
     }
 
-    /**
-     * Add days to the expiration date.
-     */
     public function addDays(int $days): Token
     {
         $expires = $this->expires_at ?: now();
@@ -36,9 +27,6 @@ trait InteractsWithExpiration
         return $this->expires($expires->addDays($days));
     }
 
-    /**
-     * Add hours to the expiration date.
-     */
     public function addHours(int $hours): Token
     {
         $expires = $this->expires_at ?: now();
@@ -46,9 +34,6 @@ trait InteractsWithExpiration
         return $this->expires($expires->addHours($hours));
     }
 
-    /**
-     * Add minutes to the expiration date.
-     */
     public function addMinutes(int $minutes): Token
     {
         $expires = $this->expires_at ?: now();
